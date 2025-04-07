3e2f2bafc48290d408acaaf3c239e9c229703a21
eloisacandido335 <eloisacandido335@gmail.com>
2025-04-07 04:12:33 +0000

    criacao da funcao para remover classe e inicio da logica

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
 let bill = 0
-let tipPorcentage = 0
-let numberofPeople = 0
+let tipPercentage = 0
+let numberOfPeople = 0
 let buttonSelected = null
 
 function receiveBillValue() {
     bill = document.querySelector("#bill").valueAsNumber
-
-    console.log(bill)
 }
 
 function receiveNumberOfPeopleValue() {
     numberOfPeople = document.querySelector("#people").valueAsNumber
 }
 
-function receiveTipPorcentageValue(value) {
-    tipPorcentage = value / 100
+function receiveTipPercentageValue(value) {
+    tipPercentage = value / 100
+
+    removeClassButtonSelected()
 
     buttonSelected = document.querySelector(`#button-${value}`)
     buttonSelected.classList.add("button-selected")
+}
 
+function removeClassButtonSelected() {
+    if (buttonSelected !== null) {
+        buttonSelected.classList.remove("button-selected")
+        buttonSelected = null
+    }
 }
